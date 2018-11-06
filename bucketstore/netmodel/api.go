e9e1a6b517f2048967670796291a9885cb74d89f
Simon Schmidt <simon.schmidt@htp-tel.de>
2018-11-06 22:41:01 +0100

    bucket store

---
+++ bucketstore/netmodel/api.go
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2018 Simon Schmidt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package netmodel
+
+import "github.com/maxymania/fastnntp-polyglot-labs2/bucketstore"
+
+type BucketType uint
+const (
+	// If the bucket is not served from any node.
+	// Every bucket in this state is effectively non-existing.
+	None BucketType = iota
+	
+	// If the bucket is served from one node.
+	One
+	
+	// If the bucket is served from more than one node.
+	Some
+	
+	// If the bucket is s
+	All
+)
+
+type Server struct {
+	Reader bucketstore.BucketR
+	Writer bucketstore.BucketW
+}
+
+type Holder interface {
+	GetHolder(bucket []byte) (srv Server,t BucketType)
+}
+
